a06faa83c8d6fba50bf3300dbcf51ea42e8233ce
Tashtemirov <samsung.galaxe2727sh@gmail.com>
2026-03-26 09:49:11 +0600

    Лабараторная №9: добавлен CI/CD пайплайн

---
+++ main.py
@@ -0,0 +1,26 @@
+
+
+def add(a, b):
+    return  a + b
+
+
+def test_add_succes():
+    assert add(2, 3) == 5
+
+
+def test_add_wrong():
+    assert add(2, 2) != 5
+
+
+def get_user_number():
+    try:
+        num = int(input("Введите число "))
+        num2 = int(input("Введите число на которой хотите делить "))
+        print(f"Ваше число: {num / num2}")
+    except ZeroDivisionError:
+        print("Ошибка! деление на ноль невозможно")
+    except ValueError:
+        print("Ошибка! Пажалуйста, введите только число, а не буквы.")
+
+if __name__ == "__main__":
+    get_user_number()
