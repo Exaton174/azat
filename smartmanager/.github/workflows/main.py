ea07ea2fd215429d01c2c8c2c4b9446ddeeddc78
Tashtemirov <samsung.galaxe2727sh@gmail.com>
2026-04-16 09:53:42 +0600

    fix

--- smartmanager/.github/workflows/main.py
+++ smartmanager/.github/workflows/main.py
@@ -3,7 +3,7 @@ def add(a, b):
 
 def divide(a, b):
     if b == 0:
-        raise ValueError("Деление на ноль невозможно!")
+        raise ValueError("Cannot divide by zero")
     return a / b
 
 def is_even(n):
